cc82d3262bce9ef095b73a86d3fb5f0f6e3da3f7
Fathima Nasrin <er.fathimanasrin@gmail.com>
2023-11-08 19:17:06 +0530

    git first push in new repo

---
+++ middleware/userAuth.js
@@ -0,0 +1,32 @@
+
+const islogin=async(req,res,next)=>{
+    if(req.session.user){
+        next()
+    }else{
+        res.redirect("/")
+    }
+    
+}
+const iflogin=async(req,res,next)=>{
+    if(req.session.user){
+        res.redirect("/home")
+    }else{
+        next()
+    }
+}
+
+const islogout=async(req,res,next)=>{
+    console.log("logout middleware")
+    req.session.user=null;
+    next()
+}
+
+const cartAuth=async(req,res,next)=>{
+    if(req.session.user){
+        next()
+    }else{
+        res.redirect("/login")
+    }
+}
+
+module.exports={islogout,islogin,cartAuth,iflogin}
